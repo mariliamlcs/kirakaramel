509713c6e027d8f32c5d627a29cd8997fcda9a28
Lia <malachiasmarilia@gmail.com>
2022-05-03 17:57:11 -0300

    create faq

--- src/pages/Faq/index.js
+++ src/pages/Faq/index.js
@@ -1,9 +1,21 @@
+import './index.css';
+import estrelinha from '../../assets/estrelinha.png';
+
 export default function Faq(){
     return(
     <> 
-        <div className="content">
-        <h1>Page FAQ</h1>
+    <div className="content">
+        <div className='title-sections'>
+        <div className='estrelinha'><img src={estrelinha}/></div>
+        <h1 className='faq-title title-sections orange-text'>
+        F.A.Q&nbsp;<b>Kira Kara</b></h1>
+        <div className='estrelinha'><img src={estrelinha}/></div>
         </div>
+        <br/>
+        <hr className='linha-marrom'/>
+        <hr className='linha-rose'/>
+
+    </div>
     </>
     )
 }
