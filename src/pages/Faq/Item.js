26e28fa32e3b55e93e9af0b51040c2962a07cd63
Lia <malachiasmarilia@gmail.com>
2022-05-14 20:35:02 -0300

    edit collapsible list CSS

--- src/pages/Faq/Item.js
+++ src/pages/Faq/Item.js
@@ -1,11 +1,12 @@
+import './index.css';
 import Collapsible from 'react-collapsible';
 
 export default function itemFAQ({question, answer}){
     return(
     <>
-     <Collapsible trigger={question}>
-            <div className='resposta'>
-                {answer}
+     <Collapsible trigger={question} className='question-faq' >
+            <div className='answer-faq'>
+            {answer}
             </div>
         </Collapsible>   
     </>
--- src/pages/Faq/index.js
+++ src/pages/Faq/index.js
@@ -15,8 +15,40 @@ export default function Faq(){
         <br/>
         <hr className='linha-marrom'/>
         <hr className='linha-rose'/>
-
-        
+        <div className='faq-container'>
+        <Item 
+            question="How much does Kira control its creators and their content?"
+            answer="Officia voluptate irure exercitation ad exercitation duis dolore reprehenderit do tempor nulla. Ea et esse cillum non deserunt dolore duis aliquip consequat non."
+        />
+        <Item 
+            question="Do you set their schedules?"
+            answer="Eiusmod dolor laborum et officia dolor aliqua elit officia cupidatat incididunt exercitation commodo sint."
+        />
+        <Item 
+            question="Did you poach or buy your creators?"
+            answer="Pariatur magna nisi dolor exercitation duis aliquip."
+        />
+        <Item 
+            question="Are you planning Generations?"
+            answer="Deserunt excepteur nulla est laboris sunt nostrud commodo irure elit minim sunt aliqua id."
+        />
+        <Item 
+            question="When did kira kara start?"
+            answer="Commodo eiusmod duis adipisicing eu laboris ea tempor."
+        />
+        <Item 
+            question="What am I allowed to upload?"
+            answer="Aute adipisicing aliqua occaecat deserunt mollit."
+        />
+        <Item 
+            question="Do we control our creators's social media?"
+            answer="Aliquip voluptate minim anim elit cupidatat aute laborum sint pariatur enim."
+        />
+        <Item 
+            question="What does kira kara provide their creators?"
+            answer="Voluptate do consequat velit eu eu commodo nostrud."
+        />
+        </div>
     </div>
     </>
     )
