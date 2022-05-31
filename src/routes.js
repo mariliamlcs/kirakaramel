98e0de76b35d219d285f4eacc5d7f358cf4384e0
Lia <malachiasmarilia@gmail.com>
2022-05-31 01:09:16 -0300

    create Sunny page and 1st container

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Faq from "./pages/Faq";
 import Shop from "./pages/Shop";
 import Streamers from "./pages/Streamers";
+import Sunny from "./pages/Streamers/Sunny";
   
 export default function Routes(){
 return(
@@ -14,6 +15,7 @@ return(
   <Route path="/faq" element={<Faq />} />
   <Route path="/shop" element={<Shop />} />
   <Route path="/streamers" element={<Streamers />} />
+  <Route path="/Sunny" element={<Sunny />} />
 </ReactRoutes>
 )
 }
